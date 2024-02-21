ec9bae09cf8a4541063208fdb6813b0da1315ae9
D D <dbd54@drexel.edu>
2024-02-21 16:26:35 -0500

    Finish connecting backend api to front end

--- backend/place.js
+++ backend/place.js
@@ -1,15 +1,9 @@
 const places = [
     {
-        name: "Saxbys Drexel University",
-        address: "65 N 34th St, Philadelphia, PA 19104",
-        description: "Relaxed coffeehouse chain offering small-batch coffees & light fare such as sandwiches.",
-        id: "84af111e-db0e-4b8e-9a7a-544208ebe541",
-    },
-    {
-        name: "7-Eleven",
-        address: "3401 Lancaster Ave, Philadelphia, PA 19104",
-        description: "Convenience chain offering grab-&-go bites & beverages, plus assorted newsstand items.",
-        id: "d8cda680-a997-48c5-b6b6-8f7c78399cde",
+        name:"Buckley Green",
+        address:"30 N 33rd St, Philadelphia, PA 19104",
+        description:"Straightforward park at Drexel University with volleyball courts, a BBQ grill & more.",
+        id:"fc262f75-4992-4d9a-8a15-df0584602eb"
     },
     {
         name: "Daskalakis Athletic Center",
@@ -18,16 +12,74 @@ const places = [
         id: "9e802b0e-ca9a-4618-bd97-23cc95e8c38f",
     },
     {
-        name: "Landmark Americana University City",
-        address: "3333 Market St, Philadelphia, PA 19104",
-        description: "Lively hangout with a menu of pub grub & cocktails, plus multiple TVs for sports.",
-        id: "540e3d38-6790-40e6-aefb-48b06717cc25",
+        name:"Drexel Park",
+        address:"3100 Powelton Ave, Philadelphia, PA 19104",
+        description:"This 2.5-acre open park area features walking paths, benches & views of the city's skyline.",
+        id:"4eb578be-f599-4c4d-9428-cb04ec032780"
     },
     {
-        name: "GIANT Heirloom Market",
-        address: "3401 Chestnut St, Philadelphia, PA 19104",
-        id: "7425a157-d699-45c9-aa04-090517223e0e",
+        name:"Drexel University Recreation Center",
+        address:"3301 Market St, Philadelphia, PA 19104",
+        description:"Members-only gym at Drexel University featuring a pool, classes, squash, a climbing wall & more.",
+        id:"08df1de7-61ba-484c-9255-45f846053a77"
     },
+    {
+        name:"Drexel Square",
+        address:"Philadelphia, PA 19104",
+        description: null,
+        id:"ad75121e-d95b-450c-bbb3-60c138f69785"
+    },
+    {
+        name:"Library Learning Terrace",
+        address:"Race Street Hall, 3300 Market St",
+        description: null,
+        id:"a10885e2-4d2a-47aa-bb52-a5abe2cbba87"
+    },
+    {
+        name:"Northside Dining Terrace",
+        address:"203 N 34th St, Philadelphia, PA 19104",
+        description: "Cafeteria",
+        id:"f1fad892-ddfd-4c23-bee1-52b5366ab376"
+    },
+    {
+        name:"Urban Eatery",
+        address:"3400 Lancaster Ave, Philadelphia, PA 19104",
+        description:"Cafeteria",
+        id:"8b29ef67-d58f-4447-9cd5-aab52e74376e"
+    },
+    {
+        name:"Hagerty Library",
+        address:"Race Street Hall, 3300 Market St, Philadelphia, PA 19104",
+        description: null,
+        id:"ff995cf7-c56f-48f3-85c1-b334ecab2a6d"
+    },
+    {
+        name:"7-Eleven",
+        address:"3401 Lancaster Ave, Philadelphia, PA 19104",
+        description:"Convenience chain offering grab-&-go bites & beverages, plus assorted newsstand items.",
+        id:"f6343079-f0f4-4718-9bb1-724d48b4b099"
+    },
+    {
+        name:"Saxbys Drexel University",
+        address:"65 N 34th St, Philadelphia, PA 19104",
+        description:"Relaxed coffeehouse chain offering small-batch coffees & light fare such as sandwiches.",
+        id:"1aa6d20d-e6c9-4cfc-9936-a25eaa69faa5"
+    },
+    {
+        name:"Old Nelson Food Company",
+        address:"3438-3448 Lancaster Ave, Philadelphia, PA 19104",
+        description: null,
+        id:"92ee19d7-6f6a-47f1-90cb-d96cf0f8beb1"
+    },
+    
+
 ]
 
 exports.places = places;
+
+// {
+//     name:"",
+//     address:"",
+//     description:"",
+//     id:""
+// },
